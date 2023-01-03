248b0bb0531292a3cd8c95b7a05a0da7aac2801d
Welisson Ferreira <106556611+WelissonFerreira@users.noreply.github.com>
2023-01-02 23:05:05 -0300

    primeiro
    primeirocomiitjs

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velo = 70
+var velopermitida = '60km'
+console.log(`A velocidade do seu carro é ${velo}`)
+if (velo > 60) {
+    console.log(`Você ultrapassou a velocidade permitida > ${velopermitida} <. MULTADO!`)
+}
+console.log('Diriga sempre usando cinto de segurança!')
